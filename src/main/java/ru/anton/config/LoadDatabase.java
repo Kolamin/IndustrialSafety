bb82b144f9a8cce3a1c88d7a8f938394324d3042
Anton Zhiliakov <gofranich@gmail.com>
2021-12-03 12:31:33 +0600

    Release final test

--- src/main/java/ru/anton/config/LoadDatabase.java
+++ src/main/java/ru/anton/config/LoadDatabase.java
@@ -15,9 +15,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Configuration
