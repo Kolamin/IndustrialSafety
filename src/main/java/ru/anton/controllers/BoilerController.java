f6ad9c6a15fc8614352818c7e9c5a9dda85ef72e
Kolamin <gofranich@gmail.com>
2021-12-14 12:45:19 +0600

    Release for beta test.

--- src/main/java/ru/anton/controllers/BoilerController.java
+++ src/main/java/ru/anton/controllers/BoilerController.java
@@ -2,8 +2,11 @@ package ru.anton.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.anton.entity.boiler.BoilerQuestions;
+import ru.anton.entity.boiler.ConcreteBoilerAnswer;
+import ru.anton.entity.industrial.CorrectIndustAnswer;
+import ru.anton.entity.industrial.IndustrialQuestions;
 import ru.anton.repository.boiler.BoilerRepository;
 import ru.anton.repository.boiler.CorrectAnswerBoilerRepo;
 
@@ -23,5 +26,25 @@ public class BoilerController {
     public String getBoilerAllQuestions(Model model){
         model.addAttribute( "questions", boilerRepository.findAll() );
         return "boilerall";
+    }
+
+    @GetMapping("/concreteboiler/{id}")
+    public String getSingleBoilerQuestion(Model model, @PathVariable("id") long id) {
+        model.addAttribute("question", boilerRepository.findById(id));
+        return "boilerQuestion";
+    }
+
+    @PostMapping("/answer/{id}")
+    public String getAnswer(Model model, @ModelAttribute BoilerQuestions answer, @PathVariable("id") long id) {
+        model.addAttribute("answer", answer);
+        ConcreteBoilerAnswer answerBoilerRepoById = correctAnswerBoilerRepo.findById(id);
+
+        StringBuilder resultAnswer = new StringBuilder();
+        for (String s : answerBoilerRepoById.getCorrectAnswer()) {
+            resultAnswer.append(s).append(",");
+        }
+        model.addAttribute("correctAnswer", resultAnswer.substring(0, resultAnswer.length() - 1));
+        model.addAttribute("id", id);
+        return "boilerAnswer";
     }
 }
